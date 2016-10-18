coralvanda/game-api

commit efe5d48d350d76bbf3f3aba35ee826b534dda7e9

Move ai fleet construction logic to models.py inside transactional function

api.py
@@ -16,7 +16,7 @@ from models import User, Game, Score, Board, Fleet, StringMessage
 from models import BoardForm, NewGameForm, GameForm, GameForms
 from models import MakeMoveForm, ScoreForms, PlaceShipForm
 
-from utils import get_by_urlsafe, ai_fleet_builder
+from utils import get_by_urlsafe
 from settings import WEB_CLIENT_ID
 
 API_EXPLORER_CLIENT_ID = endpoints.API_EXPLORER_CLIENT_ID
@@ -62,7 +62,6 @@ class BattleshipAPI(remote.Service):
                       path='game',
                       name='new_game',
                       http_method='POST')
-    @ndb.transactional(xg=True)
     def new_game(self, request):
         """Creates new game"""
         user = User.query(User.name == request.user_name).get()
@@ -70,19 +69,6 @@ class BattleshipAPI(remote.Service):
             raise endpoints.NotFoundException(
                     'A User with that name does not exist!')
         game = Game.new_game(user.key)
-        ai_fleet = game.ai_fleet.get()
-        ai_board = game.ai_board.get()
-        ai_fleet_formation = ai_fleet_builder()
-        for x in range(len(ai_fleet_formation)):
-            setattr(ai_board, 'row_' + str(x), ai_fleet_formation[x])
-        ai_fleet.carrier_status = 'placed'
-        ai_fleet.battleship_status = 'placed'
-        ai_fleet.cruiser_status = 'placed'
-        ai_fleet.submarine_status = 'placed'
-        ai_fleet.destroyer_status = 'placed'
-        ai_fleet.put()
-        ai_board.put()
-
         # Use a task queue to update the average attempts remaining.
         # This operation is not needed to complete the creation of a new game
         # so it is performed out of sequence.
@@ -111,7 +97,7 @@ class BattleshipAPI(remote.Service):
         """Return a list of active games a player is engaged in"""
         user = User.query(User.name == request.user_name).get()
         games = Game.query()
-        games = games.filter(Game.user == user.key)
+        games = games.filter(Game.user_id == user.key)
         games = games.filter(Game.game_over == False).fetch()
         return GameForms(games=[game.to_form('') for game in games])
 
@@ -144,7 +130,7 @@ class BattleshipAPI(remote.Service):
                     logging.info('ship exceeds size of board, return False')
                     return False
                 if getattr(board, 'row_' +
-                    str(request.bow_row + x))[request.bow_position] == '1':
+                    str(request.bow_row + x))[request.bow_position] != '0':
                     logging.info('ship overlaps with another, return False')
                     return False
         else:
@@ -153,7 +139,7 @@ class BattleshipAPI(remote.Service):
                     logging.info('ship exceeds size of board, return False')
                     return False
                 if getattr(board,'row_' +
-                    str(request.bow_row))[request.bow_position + x] == '1':
+                    str(request.bow_row))[request.bow_position + x] != '0':
                     logging.info('ship overlaps with another, return False')
                     return False
         logging.info('_valid_placement ends, returns True')
@@ -202,7 +188,7 @@ class BattleshipAPI(remote.Service):
             raise endpoints.BadRequestException('Invalid ship placement')
 
 
-    # ahFkZXZ-ZnNuZC1nYW1lLWFwaXIRCxIER2FtZRiAgICAgODXCww
+    # ahFkZXZ-ZnNuZC1nYW1lLWFwaXIiCxIEVXNlchiAgICAgICACgwLEgRHYW1lGICAgICA0OcLDA
 
     @endpoints.method(request_message=BOARD_REQUEST,
                     response_message=BoardForm,
@@ -270,7 +256,7 @@ class BattleshipAPI(remote.Service):
         if not user:
             raise endpoints.NotFoundException(
                     'A User with that name does not exist!')
-        scores = Score.query(Score.user == user.key)
+        scores = Score.query(Score.user_id == user.key)
         return ScoreForms(items=[score.to_form() for score in scores])
 
     '''

models.py
@@ -5,6 +5,7 @@ classes they can include methods (such as 'to_form' and 'new_game')."""
 from datetime import date
 from protorpc import messages
 from google.appengine.ext import ndb
+from utils import ai_fleet_builder
 
 
 class User(ndb.Model):
@@ -17,50 +18,68 @@ class Game(ndb.Model):
     """Game object"""
     move_count  = ndb.IntegerProperty()
     game_over   = ndb.BooleanProperty(required=True, default=False)
-    user        = ndb.KeyProperty(required=True, kind='User')
+    user_id     = ndb.KeyProperty(required=True, kind='User')
     user_fleet  = ndb.KeyProperty(kind='Fleet')
     user_board  = ndb.KeyProperty(kind='Board')
     user_chart  = ndb.KeyProperty(kind='Board')
-    AI_fleet    = ndb.KeyProperty(kind='Fleet')
-    AI_board    = ndb.KeyProperty(kind='Board')
-    AI_chart    = ndb.KeyProperty(kind='Board')
+    ai_fleet    = ndb.KeyProperty(kind='Fleet')
+    ai_board    = ndb.KeyProperty(kind='Board')
+    ai_chart    = ndb.KeyProperty(kind='Board')
 
     @classmethod
     @ndb.transactional(xg=True)
     def new_game(cls, user):
         """Creates and returns a new game"""
-        user_fleet = Fleet()
-        user_board = Board()
-        user_chart = Board()
-        AI_fleet = Fleet()
-        AI_board = Board()
-        AI_chart = Board()
+        game = Game(parent=user)
+        game.user_id=user
+        game.game_over=False
+        game_key = game.put()
+
+        user_fleet = Fleet(parent=game_key)
+        user_board = Board(parent=game_key)
+        user_chart = Board(parent=game_key)
+        ai_fleet = Fleet(parent=game_key)
+        ai_board = Board(parent=game_key)
+        ai_chart = Board(parent=game_key)
+
         user_fleet_key = user_fleet.put()
         user_board_key = user_board.put()
         user_chart_key = user_chart.put()
-        AI_fleet_key = AI_fleet.put()
-        AI_board_key = AI_board.put()
-        AI_chart_key = AI_chart.put()
-        game = Game(user=user,
-                    user_fleet=user_fleet_key,
-                    user_board=user_board_key,
-                    user_chart=user_chart_key,
-                    AI_fleet=AI_fleet_key,
-                    AI_board=AI_board_key,
-                    AI_chart=AI_chart_key,
-                    game_over=False)
+        ai_fleet_key = ai_fleet.put()
+        ai_board_key = ai_board.put()
+        ai_chart_key = ai_chart.put()
+
+        game.user_fleet = user_fleet_key
+        game.user_board = user_board_key
+        game.user_chart = user_chart_key
+        game.AI_fleet = ai_fleet_key
+        game.AI_board = ai_board_key
+        game.AI_chart = ai_chart_key
         game.put()
+
         user_board.build_board()
         user_chart.build_board()
-        AI_board.build_board()
-        AI_chart.build_board()
+        ai_board.build_board()
+        ai_chart.build_board()
+
+        ai_fleet_formation = ai_fleet_builder()
+        for x in range(len(ai_fleet_formation)):
+            setattr(ai_board, 'row_' + str(x), ai_fleet_formation[x])
+        ai_fleet.carrier_status = 'placed'
+        ai_fleet.battleship_status = 'placed'
+        ai_fleet.cruiser_status = 'placed'
+        ai_fleet.submarine_status = 'placed'
+        ai_fleet.destroyer_status = 'placed'
+        ai_fleet.put()
+        ai_board.put()
+
         return game
 
     def to_form(self, message):
         """Returns a GameForm representation of the Game"""
         form = GameForm()
         form.urlsafe_key = self.key.urlsafe()
-        form.user_name = self.user.get().name
+        form.user_name = self.user_id.get().name
         form.game_over = self.game_over
         form.message = message
         return form
@@ -80,7 +99,7 @@ class Game(ndb.Model):
         self.game_over = True
         self.put()
         # Add the game to the score 'board'
-        score = Score(user=self.user, date=date.today(), won=won,
+        score = Score(user_id=self.user_id, date=date.today(), won=won,
                       moves=self.move_count)
         score.put()
 
@@ -161,13 +180,13 @@ class Fleet(ndb.Model):
 
 class Score(ndb.Model):
     """Score object"""
-    user    = ndb.KeyProperty(required=True, kind='User')
+    user_id = ndb.KeyProperty(required=True, kind='User')
     date    = ndb.DateProperty(required=True)
     won     = ndb.BooleanProperty(required=True)
     moves   = ndb.IntegerProperty(required=True)
 
     def to_form(self):
-        return ScoreForm(user_name=self.user.get().name, won=self.won,
+        return ScoreForm(user_name=self.user_id.get().name, won=self.won,
                          date=str(self.date), moves=self.moves)
 
 

utils.py
@@ -50,51 +50,51 @@ def ai_fleet_builder():
         ['C', 'B', 'c', 'S', '0', '0', '0', '0', '0', '0']
     ]
     config_2 = [
-        ['0', 'C', '0', 'B', '0', 'c', '0', 's', '0', '0']
-        ['0', 'C', '0', 'B', '0', 'c', '0', 's', '0', '0']
-        ['0', 'C', '0', 'B', '0', 'c', '0', 's', '0', '0']
-        ['0', 'C', '0', 'B', '0', '0', '0', '0', '0', '0']
-        ['0', 'C', '0', '0', '0', '0', '0', '0', '0', '0']
-        ['0', '0', '0', '0', '0', '0', 'D', 'D', '0', '0']
-        ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0']
-        ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0']
-        ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0']
+        ['0', 'C', '0', 'B', '0', 'c', '0', 's', '0', '0'],
+        ['0', 'C', '0', 'B', '0', 'c', '0', 's', '0', '0'],
+        ['0', 'C', '0', 'B', '0', 'c', '0', 's', '0', '0'],
+        ['0', 'C', '0', 'B', '0', '0', '0', '0', '0', '0'],
+        ['0', 'C', '0', '0', '0', '0', '0', '0', '0', '0'],
+        ['0', '0', '0', '0', '0', '0', 'D', 'D', '0', '0'],
+        ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+        ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+        ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
         ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0']
     ]
     config_3 = [
-        ['0', '0', '0', '0', 'S', '0', '0', '0', '0', '0']
-        ['0', '0', '0', '0', 'S', '0', '0', '0', '0', '0']
-        ['0', '0', '0', '0', 'S', '0', '0', '0', '0', '0']
-        ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0']
-        ['B', 'B', 'B', 'B', '0', 'D', '0', 'c', 'c', 'c']
-        ['0', '0', '0', '0', '0', 'D', '0', '0', '0', '0']
-        ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0']
-        ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0']
-        ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0']
+        ['0', '0', '0', '0', 'S', '0', '0', '0', '0', '0'],
+        ['0', '0', '0', '0', 'S', '0', '0', '0', '0', '0'],
+        ['0', '0', '0', '0', 'S', '0', '0', '0', '0', '0'],
+        ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+        ['B', 'B', 'B', 'B', '0', 'D', '0', 'c', 'c', 'c'],
+        ['0', '0', '0', '0', '0', 'D', '0', '0', '0', '0'],
+        ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+        ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+        ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
         ['0', '0', '0', 'C', 'C', 'C', 'C', 'C', '0', '0']
     ]
     config_4 = [
-        ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0']
-        ['0', '0', '0', '0', 'D', '0', '0', '0', '0', '0']
-        ['0', '0', '0', '0', 'D', '0', '0', '0', '0', '0']
-        ['0', '0', '0', '0', 'B', '0', '0', '0', '0', '0']
-        ['0', '0', 'S', '0', 'B', '0', 'c', '0', '0', '0']
-        ['0', '0', 'S', '0', 'B', '0', 'c', '0', '0', '0']
-        ['0', '0', 'S', '0', 'B', '0', 'c', '0', '0', '0']
-        ['0', '0', 'C', 'C', 'C', 'C', 'C', '0', '0', '0']
-        ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0']
+        ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+        ['0', '0', '0', '0', 'D', '0', '0', '0', '0', '0'],
+        ['0', '0', '0', '0', 'D', '0', '0', '0', '0', '0'],
+        ['0', '0', '0', '0', 'B', '0', '0', '0', '0', '0'],
+        ['0', '0', 'S', '0', 'B', '0', 'c', '0', '0', '0'],
+        ['0', '0', 'S', '0', 'B', '0', 'c', '0', '0', '0'],
+        ['0', '0', 'S', '0', 'B', '0', 'c', '0', '0', '0'],
+        ['0', '0', 'C', 'C', 'C', 'C', 'C', '0', '0', '0'],
+        ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
         ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0']
     ]
     config_5 = [
-        ['0', '0', '0', '0', '0', '0', 'D', 'B', '0', '0']
-        ['0', '0', '0', '0', '0', '0', 'D', 'B', '0', '0']
-        ['0', '0', '0', '0', '0', '0', 'C', 'B', '0', '0']
-        ['0', '0', '0', '0', '0', '0', 'C', 'B', 'c', '0']
-        ['0', '0', '0', '0', '0', '0', 'C', '0', 'c', '0']
-        ['0', '0', '0', '0', '0', '0', 'C', '0', 'c', '0']
-        ['0', '0', '0', '0', '0', '0', 'C', 'S', 'S', 'S']
-        ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0']
-        ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0']
+        ['0', '0', '0', '0', '0', '0', 'D', 'B', '0', '0'],
+        ['0', '0', '0', '0', '0', '0', 'D', 'B', '0', '0'],
+        ['0', '0', '0', '0', '0', '0', 'C', 'B', '0', '0'],
+        ['0', '0', '0', '0', '0', '0', 'C', 'B', 'c', '0'],
+        ['0', '0', '0', '0', '0', '0', 'C', '0', 'c', '0'],
+        ['0', '0', '0', '0', '0', '0', 'C', '0', 'c', '0'],
+        ['0', '0', '0', '0', '0', '0', 'C', 'S', 'S', 'S'],
+        ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+        ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
         ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0']
     ]
     fleets = [config_1, config_2, config_3, config_4, config_5]

commit 768cab314546869705caa22d9ccf125c4288afaf

Continue trying to fix error introduced by commit efe5d48d350d76bbf3f3aba35ee826b534dda7e9

api.py
@@ -188,7 +188,7 @@ class BattleshipAPI(remote.Service):
             raise endpoints.BadRequestException('Invalid ship placement')
 
 
-    # ahFkZXZ-ZnNuZC1nYW1lLWFwaXIbCxIEVXNlchiAgICAgICACgwLEgRHYW1lGAEM
+    # ahFkZXZ-ZnNuZC1nYW1lLWFwaXIbCxIEVXNlchiAgICAgICACgwLEgRHYW1lGAkM
 
     @endpoints.method(request_message=BOARD_REQUEST,
                     response_message=BoardForm,

models.py
@@ -30,11 +30,10 @@ class Game(ndb.Model):
     def new_game(cls, user):
         """Creates and returns a new game"""
         new_game_id = Game.allocate_ids(size=1, parent=user)[0]
-        new_game_key = ndb.Key(Game, new_game_id, parent=user)
-        game = Game(key=new_game_key,
+        game_key = ndb.Key(Game, new_game_id, parent=user)
+        game = Game(key=game_key,
                     user_id=user,
                     game_over=False)
-        game_key = game.put()
 
         new_user_fleet_id = Fleet.allocate_ids(size=1, parent=game_key)[0]
         new_user_fleet_key = ndb.Key(Fleet, new_user_fleet_id, parent=game_key)