37700b24f5f99e7d0193c9e6b4db246252c6a9d1
Eric <coralvanda@gmail.com>
2017-03-29 13:03:00 +0200

    feat: Update user model to include a password and class methods

--- models/user.py
+++ models/user.py
@@ -1,6 +1,73 @@
 from google.appengine.ext import ndb
 
+import random
+import string
+import hashlib
+
+def make_salt(length=5):
+	"""Creates/returns a string of random letters, 5 by default"""
+	return "".join(random.choice(string.letters) for x in range(length))
+
+def make_pw_hash(name, pw, salt=None):
+	"""Uses salt to make a more secure password hash"""
+	if not salt:
+		salt = make_salt()
+	h = hashlib.sha256(name + pw + salt).hexdigest()
+	return '%s,%s' % (salt, h)
+
+def valid_pw(name, password, h):
+	"""Verifies that a password is valid using the hash"""
+	salt = h.split(',')[0]
+	return h == make_pw_hash(name, password, salt)
+
+def users_key(group = 'default'):
+	"""Returns the user's database key"""
+	#return ndb.Key.from_path('users', group)
+	return ndb.Key('users', group)
+
+
 class User(ndb.Model):
     """User profile"""
     name    = ndb.StringProperty(required=True)
+    pw_hash = ndb.StringProperty(required=True)
     email   = ndb.StringProperty()
+
+    @classmethod
+	def _by_id(cls, uid):
+		"""Takes in a user ID, returns that user if present"""
+		return User.get_by_id(uid, parent = user_key())
+
+	@classmethod
+	def _by_name(cls, name):
+		"""Takes in a user name, returns that user if present"""
+		u = User.query(User.name == name).get()
+		return u
+
+	@classmethod
+	def _register(cls, name, pw, email = None):
+		"""Takes in user registration info, returns a user
+		DB model object"""
+		pw_hash = make_pw_hash(name, pw)
+		return User(parent=users_key(),
+			name=name,
+			pw_hash=pw_hash,
+			email=email)
+
+	@classmethod
+	def _login(cls, name, pw):
+		"""Takes in login info, and returns that user if
+		the login info is valid"""
+		u = cls._by_name(name)
+		if u and valid_pw(name, pw, u.pw_hash):
+			return u
+
+'''
+
+u = User._register(user_name, password, email)
+u.put()
+cookie_val = make_secure_val(user_name)
+self.response.set_cookie('name', cookie_val)
+self.redirect('/welcome')
+
+
+'''
